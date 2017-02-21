0cf3a781720c7cef509dc67028193c4a6318f562
Eiyeron Fulmincendii <eiyeron@gmail.com>
2017-02-21 21:42:14 +0100

    Implemented prototypical VRAM memory

--- src/haxeboy/VRAM.hx
+++ src/haxeboy/VRAM.hx
@@ -1,12 +1,21 @@
 package haxeboy;
 
+import haxe.io.UInt8Array;
+
 // TODO
 class VRAM implements MemoryMappable {
+    public static inline var VRAM_TILE_BANK_SIZE:Int = 4 * 1024; // 4 kB
+    public static inline var VRAM_MAP_BANK_SIZE:Int = 1024; // 1 kB
+
+    var tile_banks:Array<UInt8Array>;
+    var map_banks:Array<UInt8Array>;
+
     /// Mockup of eventual API ///
     var accessible(get, never):Bool;
 
     public function new() {
-
+        tile_banks = [for(i in 0...2) new UInt8Array(VRAM_TILE_BANK_SIZE)];
+        map_banks = [for(i in 0...2) new UInt8Array(VRAM_MAP_BANK_SIZE)];
     }
 
     private function get_accessible():Bool {
@@ -14,22 +23,37 @@ class VRAM implements MemoryMappable {
         return false;
     }
 
-    // TODO : Get memory value
-    // If the VRAM is accessible, the VRAM is supposed to yield 0xFF, whatever
-    // was supposed to be on the given address.
     public function getValue(address:Int):Int
     {
         if(accessible) {
-            // return memory value
-            return 0;
+            if(address < 0x1800) {
+                var num_bank:Int = Std.int(address / VRAM_TILE_BANK_SIZE);
+                return tile_banks[num_bank].get(address - num_bank * VRAM_TILE_BANK_SIZE);
+            }
+            else {
+                var num_bank:Int = Std.int((address - 0x1800) / VRAM_MAP_BANK_SIZE);                
+                return map_banks[num_bank].get(address - 0x1800 - num_bank * VRAM_TILE_BANK_SIZE);
+            }
         }
         else {
             return 0xFF;
         }
     }
 
-    public function setValue(address:Int, value:Int) {
-        // TODO : fill me
+    public function setValue(address:Int, value:Int):Void
+    {
+        if(accessible) {
+            if(address < 0x1800) {
+                var num_bank:Int = Std.int(address / VRAM_TILE_BANK_SIZE);
+                tile_banks[num_bank].set(address - num_bank * VRAM_TILE_BANK_SIZE, value);
+            }
+            else {
+                var num_bank:Int = Std.int((address - 0x1800) / VRAM_MAP_BANK_SIZE);                
+                map_banks[num_bank].set(address - 0x1800 - num_bank * VRAM_TILE_BANK_SIZE, value);
+            }
+        }
+        else {
+            // Note : throw an exception?
+        }
     }
-
 }
