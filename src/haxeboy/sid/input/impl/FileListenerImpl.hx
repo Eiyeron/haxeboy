310312b90c5366d721cb9e3e87fe933b7449989d
DavisDevelopment <epikit@gmail.com>
2017-02-23 17:26:11 -0600

    Created the haxeboy.sid.input.impl.FileListenerImpl class

---
+++ src/haxeboy/sid/input/impl/FileListenerImpl.hx
@@ -0,0 +1,23 @@
+package haxeboy.sid.input.impl;
+
+import haxe.io.Bytes;
+
+class FileListenerImpl {
+	/* Constructor Function */
+	public function new():Void {
+
+	}
+
+/* === Instance Methods === */
+
+	public function init():Void {
+
+	}
+
+	/**
+	  * callback function
+	  */
+	public dynamic function onFile(data : Bytes):Void {
+		return;
+	}
+}
