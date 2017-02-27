b67e38e4556688ae3668f635f4d2cdbb29a20356
NicoM1 <nico.may99@gmail.com>
2017-02-27 11:51:25 -0800

    add RAM_BANKS

--- src/haxeboy/mbc/MBC1.hx
+++ src/haxeboy/mbc/MBC1.hx
@@ -7,17 +7,26 @@ using haxeboy.Tools;
 
 // TODO : add memory mapping support.
 class MBC1 implements MemoryMappable implements MemoryBankBased implements MBC {
-    public static inline var ROM_BANK_SIZE:Int = 16 * 1024; // 16 kB
+    public static inline var ROM_BANK_SIZE: Int = 16 * 1024; // 16 kB
+    public static inline var RAM_BANK_SIZE: Int = 8 * 1024;
 
     var banks: Array<UInt8Array>;
+    var ram_banks: Array<UInt8Array>;
 
     var real_number_of_rom_banks:Int;
 
     public var current_rom_bank(default, null):Int;
+    var current_ram_bank: Int = 0;
     var cart_inserted(get, never):Bool;
 
-    public function new() {
+    var ram_mode: Bool = false;
+
+    var supports_ram: Bool;
+
+    public function new(ram: Bool) {
+        supports_ram = ram;
         banks = [];
+        ram_banks = [];
         real_number_of_rom_banks = 0;
         current_rom_bank = 1;
     }
@@ -39,31 +48,35 @@ class MBC1 implements MemoryMappable implements MemoryBankBased implements MBC {
         if(!cart_inserted) {
             return 0;
         }
-        var num_bank:Int = Std.int(address/ROM_BANK_SIZE);
-        // If the select rom bank is over the current amount of loaded banks
-        // The math.min is useful because you can only have access (up to) two
-        // ROM banks : Bank 0 and Bank 1-127 (which is bank swappable).
-        // Thus we're capping to up to the two accessible rom banks or lower
-        // if the ROM only contains one ROM bank.
-        if(num_bank > Math.min(real_number_of_rom_banks - 1, 1)) {
-            return 0;
-        }
+        //ROM ACCESS
+        if(address.inRange(0x000, 0x7FFF)) {
+            var num_bank:Int = Std.int(address/ROM_BANK_SIZE);
+            // If the select rom bank is over the current amount of loaded banks
+            // The math.min is useful because you can only have access (up to) two
+            // ROM banks : Bank 0 and Bank 1-127 (which is bank swappable).
+            // Thus we're capping to up to the two accessible rom banks or lower
+            // if the ROM only contains one ROM bank.
+            if(num_bank > Math.min(real_number_of_rom_banks - 1, 1)) {
+                return 0;
+            }
 
-        // As said earlier, the console can only access two ROM banks at once:
-        // Bank 0 which is always accessible
-        // Bank 1-n which is accessible by inquiring the cart to switch
-        // banks.
-        if(num_bank == 0) {
-            return banks[0].get(address % ROM_BANK_SIZE);
-        } else {
-            return banks[current_rom_bank].get(address % ROM_BANK_SIZE);
+            // As said earlier, the console can only access two ROM banks at once:
+            // Bank 0 which is always accessible
+            // Bank 1-n which is accessible by inquiring the cart to switch
+            // banks.
+            if(num_bank == 0) {
+                return banks[0].get(address % ROM_BANK_SIZE);
+            } else {
+                return banks[current_rom_bank].get(address % ROM_BANK_SIZE);
+            }
+        }
+        else if(address.inRange(0xA000,0xBFFF)) {
+            var num_bank = Std.int(address/RAM_BANK_SIZE);
+            return ram_banks[num_bank].get(address % RAM_BANK_SIZE);
         }
     }
 
     public function setValue(address:Int, value:Int) {
-        var ramMode: Bool = false;
-        var ramBank: Int = 0;
-
         //RAM is READ/WRITE, up to 3 banks
         if(address.inRange(0xA000, 0xBFFF)) {
             //RAM, should be handled in MBC
@@ -82,9 +95,9 @@ class MBC1 implements MemoryMappable implements MemoryBankBased implements MBC {
             current_rom_bank = (current_rom_bank & (0x60)) | (value & 0x1F);
         }
         else if(address.inRange(0x4000, 0x5FFF)) {
-            if(ramMode) {
+            if(ram_mode) {
                 //set 2 bit ram bank selection
-                ramBank = value & 0x3;
+                current_ram_bank = value & 0x3;
             }
             else {
                 //set upper 2 bits of rom bank selection @EIYERON AGAIN PLEASE CHECK MATH
@@ -94,10 +107,10 @@ class MBC1 implements MemoryMappable implements MemoryBankBased implements MBC {
         //swap between ROM bank addressing and RAM bank addressing
         else if(address.inRange(0x6000, 0x7FFF)) {
             if(value == 0x1) {
-                ramMode = true;
+                ram_mode = true;
             }
             else {
-                ramMode = false;
+                ram_mode = false;
             }
         }
     }
