94ab88e2b74291d011cc64eaa83c81f69ae20925
NicoM1 <nico.may99@gmail.com>
2017-02-26 17:27:01 -0800

    begin splitting into separate MBC types

---
+++ src/haxeboy/mbc/ROM.hx
@@ -0,0 +1,41 @@
+package haxeboy.mbc;
+
+import haxe.io.Bytes;
+import haxe.io.UInt8Array;
+
+using haxeboy.Tools;
+
+class ROM implements MemoryMappable {
+    var data: UInt8Array;
+    var cart_inserted: Bool = false;
+
+    public function new() {}
+
+    public function loadCart(bytes:Bytes) {
+        data = UInt8Array.fromBytes(bytes);
+        cart_inserted = true;
+    }
+
+    public function removeCart() {
+        data = null;
+        cart_inserted = false;
+    }
+
+    public function getValue(address:Int):Int {
+        if(!cart_inserted) {
+            return 0;
+        }
+
+        if(address.inRange(0x0, 0x7FFF)) {
+            return data[address];
+        }
+        else {
+            //TODO: throw error?
+            return 0;
+        }
+    }
+
+    public function setValue(address:Int, value:Int) {
+        throw 'ROM_ONLY cartridges do not support writing';
+    }
+}
